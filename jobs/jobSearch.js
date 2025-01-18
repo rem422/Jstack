db6b580cc8e31d514a0d5648d2205f6d6d65a06f
Rem Simiyu <remsimiyu@gmail.com>
2025-01-18 22:24:32 +0300

    Jstack Job Listing

---
+++ jobs/jobSearch.js
@@ -0,0 +1,35 @@
+const categories = Array.from(jCategory);
+const searchBar = document.getElementById("search_bar");
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchData = e.target.value.toLowerCase();
+    const filterData = categories.filter((item) => 
+        item.title.toLowerCase().includes(searchData)
+    );
+    displayItems(filterData);
+});
+
+
+
+
+const displayItems = (items) => {
+    const jobsContainer = document.querySelector(".jobs_container");
+
+    jobsContainer.innerHTML = '';
+
+    items.forEach((item) => {
+        const {index, image, title, rate, availability} = item;
+        const jList = document.createElement('div');
+        jList.className = 'jlist';
+        jList.innerHTML = `
+                <img src="${image}" alt="${title}" />
+                <h3>${title}</h3>
+                <p>${rate}</p>
+                <p id='key'>${availability}</p>
+        `;
+
+        jobsContainer.appendChild(jList);
+    });
+};
+
+displayItems(categories);
--- jobs/job_list.js
+++ jobs/job_list.js
@@ -1,9 +1,37 @@
 const jCategory = [
-    {
-        index: 0,
-        image: "../images/google.png",
-        title: 'Web Developer',
-        rate: "$900 - 1200/m",
-        availability: "Full Time",
-    },
-]
+  {
+    index: 0,
+    image: "../images/google.png",
+    title: "Web Developer",
+    rate: "$900 - 1200/m",
+    availability: "Full Time",
+  },
+  {
+    index: 1,
+    image: "../images/uber.png",
+    title: "Freelancer",
+    rate: "$900 - 1200/m",
+    availability: "Freelancer",
+  },
+  {
+    index: 2,
+    image: "../images/linkedin.png",
+    title: "Business Associate",
+    rate: "$900 - 1200/m",
+    availability: "Part Time",
+  },
+  {
+    index: 3,
+    image: "../images/facebook.png",
+    title: "Digital Marketing",
+    rate: "$900 - 1200/m",
+    availability: "Full Time",
+  },
+  {
+    index: 4,
+    image: "../images/yahoo.png",
+    title: "User Experience",
+    rate: "$900 - 1200/m",
+    availability: "Part Time",
+  },
+];
