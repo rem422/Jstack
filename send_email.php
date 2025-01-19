5757202547ec7229adc67af6c098b4622a6b67f0
Rem Simiyu <remsimiyu@gmail.com>
2025-01-19 21:12:53 +0300

    Jstack Job Portal

--- send_email.php
+++ send_email.php
@@ -1,18 +1,18 @@
 <?php 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+if($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-    $to = 'remsimiyu@gmail.com';
-    $subject = 'New contact form submission';
-    $headers = "From: "  $name . "<" . $email . ">"\r\n;
-    $headers = 'Replay-To: ' . $email . "\r\n";
-    $headers = "Content-Type:  text/plain; charset=utf-8 \r\n";
+    $to = "remsimiyu@gmail.com";
+    $subject = "New contact form submission";
+    $headers = "From: " . $name . " <" . $email . ">\r\n";
+    $headers .= "Replay-To: " . $email . "\r\n";
+    $headers .= "Content-Type:  text/plain; charset=utf-8 \r\n";
 
     if(mail($to, $subject, $message, $headers)) {
         http_response_code(200);
-        echo "Email sent successfully";
+        echo "Email sent successfully!";
     } else {
         http_response_code(500);
         echo "Oops! Something went wrong";
@@ -21,6 +21,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     http_response_code(403);
     echo "Access denied";
 }
-
-
 ?>
